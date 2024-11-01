0c710dcaa5911993e8bf13ad41847bbbf442c723
KHT369 <ktsao@chicagobooth.edu>
2024-11-01 20:46:43 +0000

    save 2

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -36,12 +36,32 @@ class ZebraController < ApplicationController
     render({ :template => "game_templates/play_paper"})
   end
 
+  def roll(ai_choice)
+    if ai_choice == "scissors"
+      pp "We lost!"
+    elsif ai_choice == "paper"
+      pp "We tied!"
+    else
+      pp "We won!"
+    end
+  end
+
   def lion 
     ai_choice = random
   
     @outcome = "We played scissors!\nThey played #{ai_choice}!\n #{roll(ai_choice)}"
 
     render({ :template => "game_templates/play_scissors"})
+  end
+
+  def roll(ai_choice)
+    if ai_choice == "scissors"
+      pp "We tied!"
+    elsif ai_choice == "paper"
+      pp "We won!"
+    else
+      pp "We lost!"
+    end
   end
 
 
