cc87b0c226821d7aedb3e2d51289443ff8b64d40
KHT369 <ktsao@chicagobooth.edu>
2024-11-01 19:25:21 +0000

    save1

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,15 +1,48 @@
 class ZebraController < ApplicationController
   def rules
-    render({ :template = > "layouts/rules"})
-    
+    render({ :template => "layouts/rules"})
+
   end
   
+  def random 
+    rps = ["rock", "paper", "scissors"]
+    rps.sample
+  end
+
   def giraffe
-    render({ :template => "game_templates/play_rock" })
-    @random_move = ["rock", "paper", "scissors"].sample
     
+    ai_choice = random
+  
+    @outcome = "We played rock!\nThey played #{ai_choice}!\n #{roll(ai_choice)}"
+    
+    render({ :template => "game_templates/play_rock" })
+  end
+
+  def roll(ai_choice)
+    if ai_choice == "paper"
+      pp "We lost!"
+    elsif ai_choice == "rock"
+      pp "We tied!"
+    else
+      pp "We won!"
+    end
   end
 
   def elephant 
-    render({ :template = > "game_templates/play_paper"})
+    ai_choice = random
+  
+    @outcome = "We played paper!\nThey played #{ai_choice}!\n #{roll(ai_choice)}"
+
+    render({ :template => "game_templates/play_paper"})
+  end
+
+  def lion 
+    ai_choice = random
+  
+    @outcome = "We played scissors!\nThey played #{ai_choice}!\n #{roll(ai_choice)}"
+
+    render({ :template => "game_templates/play_scissors"})
+  end
+
+
 end
